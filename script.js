8c5d22ae0b0e525f52436a7e24ddb2ca79cb856f
Milene Azevedo <mile767676@gmail.com>
2026-06-18 14:48:03 -0300

    Fetch participantes from API and display in gallery

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const participantes = [
-  {
-    nome: "Teste 1",
-    foto: "https://picsum.photos/300/400"
-  },
-  {
-    nome: "Teste 2",
-    foto: "https://picsum.photos/301/400"
-  }
-];
+const URL_API = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnSfgIlLl1OPqCTTiUL3o3LRBT2_jrVDpitLsuca_Q9oQkmAHtHvAv3sFY1H8Yp7f1RHFZ3LWRqp3Kf3LIlPyd3ubGJJclZFoG9NJu5WfI8p0EKo7GeaJ2ZDI2_tmftfbvBd__SbhlnZe_XPEJlAAqiT65ixbgr-AI7QPj3ozwH7NvX_kckX-U-rrt5HHyBZBi4SdFxrUtwTEi_1IMsPcnnf0pJHdIIunPJuvz6nXSoSwQlkYx0&lib=MTD_tgdhCgjKJ_vjFY4S5yA9neD2OrHpD";
 
-const galeria = document.getElementById("galeria");
+fetch(URL_API)
+    .then(response => response.json())
+    .then(participantes => {
 
-participantes.forEach(pessoa => {
-  galeria.innerHTML += `
-    <div class="card">
-      <img src="${pessoa.foto}">
-      <h3>${pessoa.nome}</h3>
-    </div>
-  `;
-});
+        const galeria =
+            document.getElementById("galeria");
+
+        participantes.forEach(pessoa => {
+
+            galeria.innerHTML += `
+                <div class="card">
+                    <img src="${pessoa.foto}">
+                    <h3>${pessoa.nome}</h3>
+                </div>
+            `;
+
+        });
+
+    })
+    .catch(erro => {
+        console.error("Erro:", erro);
+    });
