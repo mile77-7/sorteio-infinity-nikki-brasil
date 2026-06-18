d95d1a67a7aa5835a02101ce4fe3b50cc5f4cb80
Milene Azevedo <mile767676@gmail.com>
2026-06-18 14:55:58 -0300

    Update API URL in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const URL_API = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnSfgIlLl1OPqCTTiUL3o3LRBT2_jrVDpitLsuca_Q9oQkmAHtHvAv3sFY1H8Yp7f1RHFZ3LWRqp3Kf3LIlPyd3ubGJJclZFoG9NJu5WfI8p0EKo7GeaJ2ZDI2_tmftfbvBd__SbhlnZe_XPEJlAAqiT65ixbgr-AI7QPj3ozwH7NvX_kckX-U-rrt5HHyBZBi4SdFxrUtwTEi_1IMsPcnnf0pJHdIIunPJuvz6nXSoSwQlkYx0&lib=MTD_tgdhCgjKJ_vjFY4S5yA9neD2OrHpD";
+const URL_API = "https://script.google.com/macros/s/AKfycbyBYI2Mc6KnNA14UVEKifMLvhL-FC4ygGTqTpwwztfv/dev";
 
 fetch(URL_API)
     .then(response => response.json())
