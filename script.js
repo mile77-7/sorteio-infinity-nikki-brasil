48779d338ea51ecf40930e286f09c9e080fffc99
Milene Azevedo <mile767676@gmail.com>
2026-06-18 14:43:33 -0300

    Add participant cards to the gallery

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const participantes = [
+  {
+    nome: "Teste 1",
+    foto: "https://picsum.photos/300/400"
+  },
+  {
+    nome: "Teste 2",
+    foto: "https://picsum.photos/301/400"
+  }
+];
 
+const galeria = document.getElementById("galeria");
+
+participantes.forEach(pessoa => {
+  galeria.innerHTML += `
+    <div class="card">
+      <img src="${pessoa.foto}">
+      <h3>${pessoa.nome}</h3>
+    </div>
+  `;
+});
